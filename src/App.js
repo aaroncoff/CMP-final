0922bf25d1f6be9fcda3cbb16c01058a965a7f22
Aaron Coffey <aaroncoff@gmail.com>
2019-04-09 18:14:11 -0700

    attempt to reconstruct front-end of custommetalproducts

--- src/App.js
+++ src/App.js
@@ -1,28 +1,86 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link } from 'react-router-dom';
+import routes from './routes';
+import routes2 from './routes2';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { isDropdownEnabled: false };
+    this.dropdownClicked = this.dropdownClicked.bind(this);
+    this.onWindowClicked = this.onWindowClicked.bind(this);
+  }
+
+  componentDidMount() {
+    window.onclick = this.onWindowClicked;
+  }
+
+  dropdownClicked() {
+    let { isDropdownEnabled } = this.state;
+    this.setState({ isDropdownEnabled: !isDropdownEnabled });
+  }
+
+  onWindowClicked(event) {
+      if (!event.target.matches('.dropbtn')) {
+          this.setState({ isDropdownEnabled: false });
+      }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="topparent">
+          <img src={require('./Media/Images/banner3.jpg')} alt={'hero'} className='hero'></img>
+          <div className="topchild">
+          <a href='https://www.facebook.com/Custom-Metal-Products-426651564470418/' className='facebook'><img src={require('./Media/Images/fb-white.png')}/></a>
+          </div>
+          
+        </div>
+
+       
+        <div className="navparent">
+          <div className="navchild">
+            <input id="toggle-menu" type="checkbox" />
+            <label htmlFor="toggle-menu" className="menu-on">MENU</label>
+            <label htmlFor="toggle-menu" className="menu-off"></label>
+            <nav className='primary-nav'>
+              <Link to='/'>HOME</Link>
+              <Link to='/about'>ABOUT US</Link>
+              <div className="dropdown dd-list">
+                <button onClick={this.dropdownClicked} className="dropbtn">Products</button>
+                <div id="myDropdown" className={`dropdown-content ${this.state.isDropdownEnabled ? 'show' : ''}`}>
+                  <Link to='/glassracks'>Glass Racks</Link>
+                  <Link to='/metalfab'>Metal Fabrication</Link>
+                </div>
+              </div>
+              <a className='revText' href='https://goo.gl/cB5xd9'>LEAVE A REVIEW</a>
+              <Link to='/contact'>CONTACT US</Link>
+            </nav>
+          </div>
+        </div>
+        <div className="routes">
+          {routes}
+        </div>
+        <div className="bodyparent">
+          <div className="bodychild">
+            {routes2}
+          </div>
+        </div>
+        <div className="footerparent">
+          <div className="footerchild">
+          </div>
+          <div className="contactInfo">
+
+            <a href="tel:+16784268802" className="phone">678-426-8802</a>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
---
+++ src/routes2.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+// import About from './Components/About';
+import Home from './Home';
+// import Contact from './Components/Contact';
+import GlassRackGal from './Components/Galleries/GlassRackGal';
+import MetalFabGal from './Components/Galleries/MetalFabGal';
+// import Review from './Components/Review';
+
+const routes2 = (
+    <Switch>
+
+        {/* <Route path='/about' component={About}/>
+        <Route path='/contact' component={Contact}/> */}
+        <Route path='/glassracks' component={GlassRackGal}/>
+        <Route path='/metalfab' component={MetalFabGal}/>
+        {/* <Route path='/reviews' component={Review}/> */} */}
+
+
+
+        
+
+
+    </Switch>
+);
+
+        //Products - glass racks - metal fabrication
+        //make a payment (stripe or square)
+        //Contact us (nodemailer)
+export default routes2;
